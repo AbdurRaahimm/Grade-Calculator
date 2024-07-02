e3180bd5106f0a0025d12fa94cb6c9fac9a31c3c
“AbdurRaahimm” <rahim703936@gmail.com>
2024-07-02 15:50:42 +0600

    feat: PWA featue add for native app

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,69 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import Pages from 'vite-plugin-pages';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    Pages()
+    Pages(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        "name": "Grade Calculator",
+        "short_name": "Grade Calculator",
+        "theme_color": "#fff",
+        "background_color": "#fff",
+        "display": "standalone",
+        "orientation": "portrait",
+        "scope": "/",
+        "start_url": "/",
+        "icons": [
+          {
+            "src": "public/images/icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "public/images/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
   ],
 })
 
